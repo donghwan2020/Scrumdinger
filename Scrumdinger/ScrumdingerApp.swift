356f1b7a3d56b913971bc32229315512c33b3645
donghwan2020 <67085994+donghwan2020@users.noreply.github.com>
2021-07-25 14:46:46 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Donghwan Kim on 2021/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
