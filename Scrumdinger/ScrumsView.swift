fd4d333e453b0df8b81802475210eac37a2ab6e4
donghwan2020 <okid2318@gmail.com>
2021-08-03 01:16:26 +0900

    feat : add color file

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Donghwan Kim on 2021/08/03.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums, id: \.title) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.color)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.data)
+    }
+}
