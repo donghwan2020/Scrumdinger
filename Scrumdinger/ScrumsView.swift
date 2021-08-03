bffeb200ba00e9c8610fb2b94ad8c63e32929f10
donghwan2020 <okid2318@gmail.com>
2021-08-04 02:17:08 +0900

    feat : set starting page

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -12,7 +12,7 @@ struct ScrumsView: View {
     
     var body: some View {
         List {
-            ForEach(scrums, id: \.title) { scrum in
+            ForEach(scrums) { scrum in
                 CardView(scrum: scrum)
                     .listRowBackground(scrum.color)
             }
@@ -25,3 +25,4 @@ struct ScrumsView_Previews: PreviewProvider {
         ScrumsView(scrums: DailyScrum.data)
     }
 }
+;
