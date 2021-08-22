ceee26ad7ccbcc310e0de8719b73c7bf72d72314
donghwan2020 <okid2318@gmail.com>
2021-08-22 17:04:45 +0900

    feat : add Data struct

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -1,10 +1,3 @@
-//
-//  DailyScrum.swift
-//  Scrumdinger
-//
-//  Created by Donghwan Kim on 2021/07/27.
-//
-
 import SwiftUI
 
 struct DailyScrum: Identifiable {
@@ -31,5 +24,18 @@ extension DailyScrum {
             DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, color: Color("App Dev")),
             DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Donny", "Karla", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 1, color: Color("Web Dev"))
         ]
+    }
+}
+
+extension DailyScrum {
+    struct Data {
+        var title: String = ""
+        var attendees: [String] = []
+        var lengthInMinutes: Double = 5.0
+        var color: Color = .random
+    }
+    
+    var data: Data {
+        return Data(title: title, attendees: attendees, lengthInMinutes: Double(lengthInMinutes), color: color)
     }
 }
